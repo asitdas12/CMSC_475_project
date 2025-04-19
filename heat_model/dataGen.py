11268bf9fd71bcd68486ea7b2578461365000e9b
Jonathan Miller <74686891+jonsothervoices@users.noreply.github.com>
2025-04-18 22:45:53 -0400

    datagen working

--- heat_model/dataGen.py
+++ heat_model/dataGen.py
@@ -4,9 +4,7 @@ import os
 from scipy.ndimage import gaussian_filter
 from tqdm import tqdm
 import os
-
-os.chdir(os.path.dirname(__file__))
-
+os.chdir('C:/Users/jmmil/workspace/school/475/project')
 # === Initial condition patterns ===
 
 def random_gaussian_sum(nx, ny, num_blobs=3):
@@ -108,7 +106,7 @@ dx = 1.0 / (nx - 1)
 dy = 1.0 / (ny - 1)
 dt = 0.01
 nt = 1000
-T = 200
+T = 20
 
 alpha = 0.001
 
